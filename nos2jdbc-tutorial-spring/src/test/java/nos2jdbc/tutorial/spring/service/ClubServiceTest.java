bed593b87cc19c9459a902dc3c7733b65bdc477a
igari <igari.y@gmail.com>
2021-10-18 17:04:53 +0900

    initial

---
+++ nos2jdbc-tutorial-spring/src/test/java/nos2jdbc/tutorial/spring/service/ClubServiceTest.java
@@ -0,0 +1,27 @@
+package nos2jdbc.tutorial.spring.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import nos2jdbc.tutorial.spring.entity.Club;
+
+/**
+ * {@link ClubService}のテストクラスです。
+ * 
+ */
+@SpringBootTest
+@Transactional
+public class ClubServiceTest {
+
+    @Autowired
+    private ClubService clubService;
+
+    @Test
+    public void insert() {
+        Club c = new Club();
+        c.name = "test";
+        clubService.insert(c);
+    }
+}
