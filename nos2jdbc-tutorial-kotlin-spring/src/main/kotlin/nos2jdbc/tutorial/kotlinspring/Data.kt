fa1a47665f63715bf974682e03e548afac0febf9
IGARI <igari.y@gmail.com>
2025-02-10 17:18:13 +0900

    add upsert()

--- nos2jdbc-tutorial-kotlin-spring/src/main/kotlin/nos2jdbc/tutorial/kotlinspring/Data.kt
+++ nos2jdbc-tutorial-kotlin-spring/src/main/kotlin/nos2jdbc/tutorial/kotlinspring/Data.kt
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 import nos2jdbc.tutorial.kotlinspring.service.ClubService
+import nos2jdbc.tutorial.kotlinspring.entity.Club
 
 @Component
 public class Data(var clubService: ClubService) {
@@ -25,9 +26,22 @@ public class Data(var clubService: ClubService) {
     fun insert() {
         if (clubService.findAll().size > 0) return;
         val implementor = clubService.jdbcManager as JdbcManagerImplementor
-        val dataSet:DataSet = SsReader("init.xlsx", true).read();
+        val dataSet:DataSet = SsReader("init.xlsx", true).read()
 
-        val dataSource: DataSource = implementor.getDataSource();
-        SqlWriter(dataSource).write(dataSet);
+        val dataSource: DataSource = implementor.getDataSource()
+        SqlWriter(dataSource).write(dataSet)
+
+        upsert()
     }
+
+    @Transactional
+    fun upsert() {
+        var c: Club = clubService.findById(1L)
+        c.name = "硬式野球部"
+        clubService.upsert(c)
+        c.id = null
+        c.name = "美術部"
+        clubService.upsert(c)
+    }
+
 }
